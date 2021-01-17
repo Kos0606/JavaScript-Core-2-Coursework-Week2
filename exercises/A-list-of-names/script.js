504587716e828fc39480b4b90ab9255bb4cd3e12
Kos0606 <65927936+Kos0606@users.noreply.github.com>
2021-01-17 19:46:11 +0000

    Javascript-Core-2-Coursework-Week 2

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,7 +1,15 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
-}
 
+for(let x of arrayOfPeople) {
+  let addName = document.createElement("h1");
+  addName.innerText = `${x.name}`;
+  content.appendChild(addName);
+  let addJob = document.createElement("h2");
+  addJob.innerText = `${x.job}`;
+  content.appendChild(addJob);
+}
+}
 let people = [
   { name: "Chris", job: "Teacher" },
   { name: "Joanna", job: "Student" },
