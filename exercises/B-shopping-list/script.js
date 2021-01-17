5d59ecd409a89dab8ec2dc59f9e3e49cb4d361b6
Kos0606 <65927936+Kos0606@users.noreply.github.com>
2021-01-17 20:52:38 +0000

    JavaScript-Core-2-Coursework-Week2

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,12 @@
 function shoppingList(arrayOfPeople) {
   // Write your code here...
+  let content = document.querySelector("#content");
+  for(let y of arrayOfPeople) {
+    let createUnOderedListTag = document.createElement("ul");
+    let createListTag = document.createElement("li");
+    createListTag.innerText = `${y}`;
+    content.appendChild(createListTag);
+  }
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
